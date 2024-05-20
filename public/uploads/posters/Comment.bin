b28dd1341e9eb6a0ac9daeaad05b4827533816d9
tbtaleb* <tbfeeber@gmail.com>
2024-05-20 18:38:37 +0100

    umm ehh ki nhabet 7aja fl dashboard temchi ll front zeda

---
+++ public/uploads/posters/Comment.bin
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ApiResource()]
+
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contenu = null;
+
+    #[ORM\Column(type: "datetime")]
+    private ?\DateTimeInterface $published = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    private ?Post $post = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function __construct()
+    {
+        $this->published = new \DateTime();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getPublished(): ?\DateTimeInterface
+    {
+        return $this->published;
+    }
+
+    public function setPublished(\DateTimeInterface $published): static
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Post $post): static
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
--- src/Controller/Admin/PosterCrudController.php
+++ src/Controller/Admin/PosterCrudController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller\Admin;
 
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use App\Entity\Poster;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class PosterCrudController extends AbstractCrudController
 {
+    private $parameterBag;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Poster::class;
@@ -22,27 +33,56 @@ class PosterCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            ImageField::new('posterFile')
-                ->setLabel('Poster')
-                ->setBasePath('uploads/posters')
-                ->setUploadDir('public/uploads/posters') // Path relative to your project root
-                ->setRequired(false), // Optional: Set to true if the image is required
             TextField::new('album'),
             TextField::new('artist'),
             IntegerField::new('quantity'),
             DateTimeField::new('added_at')->setFormat('Y-MM-dd HH:mm:ss'),
             DateTimeField::new('updated_at')->setFormat('Y-MM-dd HH:mm:ss'),
+            ImageField::new('posterFile')
+            ->setLabel('posterFile')
+            ->setBasePath('/uploads/posters')
+            ->onlyOnIndex(),
+            // Add the fileupload field
+            TextField::new('posterFile', 'Upload File')
+                ->setFormType(FileType::class)
+                ->setFormTypeOption('mapped', false)
+                ->onlyOnForms(),
         ];
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $this->handleFileUpload($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
     }
-    */
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->handleFileUpload($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function handleFileUpload(Poster $post): void
+    {
+        $uploadedFile = $this->getContext()->getRequest()->files->get('Poster')['posterFile'];
+        if ($uploadedFile instanceof UploadedFile) {
+            $fileName = md5(uniqid()) . '.' . $uploadedFile->guessExtension();
+            $symfonyUploadsDir = $this->parameterBag->get('uploads_directory');
+            $angularAssetsDir = $this->parameterBag->get('angular_assets_directory');
+
+            // Move file to Symfony uploads directory
+            $uploadedFile->move($symfonyUploadsDir, $fileName);
+
+            // Ensure the Angular assets directory exists
+            $filesystem = new Filesystem();
+            if (!$filesystem->exists($angularAssetsDir)) {
+                $filesystem->mkdir($angularAssetsDir, 0777);
+            }
+
+            // Copy file to Angular assets directory
+            $filesystem->copy($symfonyUploadsDir . '/' . $fileName, $angularAssetsDir . '/' . $fileName);
+
+            $post->setPosterFile($fileName);
+        }
+    }
 }
